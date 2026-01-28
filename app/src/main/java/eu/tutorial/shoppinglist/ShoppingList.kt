3f9d4f83d2e6c02d34b1ef1c38dcb114252e86c2
andreasZel <antreaszel@gmail.com>
2026-01-29 00:12:07 +0200

    added edit/delete functionality

--- app/src/main/java/eu/tutorial/shoppinglist/ShoppingList.kt
+++ app/src/main/java/eu/tutorial/shoppinglist/ShoppingList.kt
@@ -13,8 +13,14 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Clear
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -24,15 +30,19 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.BlendMode.Companion.Color
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 
 data class ShoppingItem(
     val id: Int,
     var name: String,
-    var quantity: Int,
-    var isEditing: Boolean = false
+    var quantity: Int
+);
+
+data class EditHandler(
+    var isOpen: Boolean = false,
+    var state: String = "create",
+    var selectedItemId: Int? = null
 );
 
 @Composable
@@ -43,19 +53,32 @@ fun ListItem(item: ShoppingItem, onEditClick: () -> Unit, onDeleteClick: () -> U
             .padding(8.dp)
             .fillMaxWidth()
             .border(
-                border = BorderStroke(2.dp, Color(0x26D4FF))
+                border = BorderStroke(2.dp, Color(0xff64b5f6)),
+                shape = RoundedCornerShape(20)
             )
     ) {
-        Text(item.name)
-        Spacer(modifier = Modifier.width(8.dp))
-        Text(item.quantity.toString())
+        Row {
+            Text("Name: ${item.name}")
+            Spacer(modifier = Modifier.width(8.dp))
+            Text("Quantity: ${item.quantity.toString()}")
+        }
+        Row(modifier = Modifier.padding(8.dp)) {
+            IconButton(onClick = onEditClick) {
+                Icon(imageVector = Icons.Default.Edit, contentDescription = "Edit Icon")
+            }
+            IconButton(onClick = onDeleteClick) {
+                Icon(imageVector = Icons.Default.Clear, contentDescription = "Delete Icon")
+            }
+        }
     }
 }
 
 @Composable
 fun ShoppingListApp() {
     var shopping_items by remember { mutableStateOf(listOf<ShoppingItem>()) }
-    var showDialog by remember { mutableStateOf(false) }
+    var isDialogOpen by remember { mutableStateOf(false) }
+    var dialogState by remember { mutableStateOf("create") }
+    var selectedItemId: Int? by remember { mutableStateOf(null) }
     var inputValueName by remember { mutableStateOf("") }
     var inputValueQuantity by remember { mutableStateOf("") }
 
@@ -68,7 +91,24 @@ fun ShoppingListApp() {
             )
 
             shopping_items = shopping_items + newItem
-            showDialog = false
+            isDialogOpen = false
+            inputValueName = ""
+            inputValueQuantity = ""
+        }
+    }
+
+    fun saveChanges() {
+        if (inputValueName != "" && inputValueQuantity != "" && selectedItemId != null) {
+
+            shopping_items = shopping_items.map { item ->
+                if (item.id == selectedItemId) {
+                    item.copy(
+                        name = inputValueName,
+                        quantity = inputValueQuantity.toIntOrNull() ?: 0
+                    )
+                } else item
+            }
+            isDialogOpen = false
             inputValueName = ""
             inputValueQuantity = ""
         }
@@ -81,7 +121,10 @@ fun ShoppingListApp() {
         verticalArrangement = Arrangement.Center
     ) {
         Button(
-            onClick = { showDialog = true },
+            onClick = {
+                dialogState = "create"
+                isDialogOpen = true
+            },
             modifier = Modifier.align(Alignment.CenterHorizontally)
         ) {
             Text("Add an Item")
@@ -92,14 +135,24 @@ fun ShoppingListApp() {
                 .padding(16.dp)
         ) {
             items(shopping_items) {
-                ListItem(it, {}, {})
+                ListItem(
+                    it,
+                    {
+                        inputValueName = it.name
+                        inputValueQuantity = it.quantity.toString()
+                        dialogState = "edit"
+                        isDialogOpen = true
+                        selectedItemId = it.id
+                    }, {
+                        shopping_items = shopping_items.minusElement(it)
+                    })
             }
         }
     }
 
-    if (showDialog) {
+    if (isDialogOpen) {
         AlertDialog(
-            onDismissRequest = { showDialog = false },
+            onDismissRequest = { isDialogOpen = false },
             confirmButton = {
                 Row(
                     Modifier
@@ -107,15 +160,24 @@ fun ShoppingListApp() {
                         .padding(8.dp),
                     horizontalArrangement = Arrangement.SpaceBetween,
                 ) {
-                    Button(onClick = { addItem() }, modifier = Modifier.padding(4.dp)) {
-                        Text("Add Item")
+                    Button(onClick = {
+                        if (dialogState == "create") {
+                            addItem()
+                        } else {
+                            saveChanges()
+                        }
+                    }, modifier = Modifier.padding(4.dp)) {
+                        Text(if (dialogState == "create") "Add Item" else "Save")
                     }
-                    Button(onClick = { showDialog = false }, modifier = Modifier.padding(4.dp)) {
+                    Button(
+                        onClick = { isDialogOpen = false; dialogState = "create" },
+                        modifier = Modifier.padding(4.dp)
+                    ) {
                         Text("Cancel")
                     }
                 }
             },
-            title = { Text("Add Shopping Item") },
+            title = { Text(if (dialogState == "create") "Add Shopping Item" else "Edit Shopping Item") },
             text = {
                 Column() {
                     Column {
